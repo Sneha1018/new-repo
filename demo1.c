eda22f8ee075f551fe64d146048ecf00550c96b7
Sneha <snehasharma101026@gmail.com>
2025-02-17 14:37:48 +0530

    Add files via upload

---
+++ demo1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,r;
+	int fact(int n);
+	printf("enter the value of n and r");
+	scanf("%d%d",&n,&r);
+	if(n<r)
+		printf("the value must be greater  than the value of r");
+	else
+		printf("Binomial coefficient=%d",fact(n)/(fact(r)*fact(n-r)));
+	getch();
+}
+//recursive function
+int fact(int n)
+{
+	if(n==0||n==1)
+		return(1);
+	else
+		return(n*fact(n-1));
+}
